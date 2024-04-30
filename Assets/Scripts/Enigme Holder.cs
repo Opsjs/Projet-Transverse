991dc45a3aa55f4cfd7469a6618424b3b4df215c
Opsjs <106817113+Opsjs@users.noreply.github.com>
2024-04-30 16:05:05 +0200

    Icones interaction

--- Assets/Scripts/Enigme Holder.cs
+++ Assets/Scripts/Enigme Holder.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class EnigmeHolder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject icon;
+    private enum Enigmes
     {
-        
+        Enigme1,
+        Enigme2,
+        Enigme3,
+        Enigme4,
     }
+    [SerializeField] private Enigmes enigme;
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        switch (enigme)
+        {
+            case Enigmes.Enigme1:
+                if (Enigme1.isCorrect)
+                {
+                    GameObject.Destroy(icon);
+                    Debug.Log($"Énigme 1 résolue");
+                }
+                break;
+            case Enigmes.Enigme2:
+                if (Enigme2.isCorrect)
+                {
+                    GameObject.Destroy(icon);
+                    Debug.Log($"Énigme 2 résolue");
+                }
+                break;
+            case Enigmes.Enigme3:
+                if (Enigme3.isCorrect)
+                {
+                    GameObject.Destroy(icon);
+                    Debug.Log($"Énigme 3 résolue");
+                }
+                break;
+            case Enigmes.Enigme4:
+                if (Enigme4.isCorrect)
+                {
+                    GameObject.Destroy(icon);
+                    Debug.Log($"Énigme 4 résolue");
+                }
+                break;
+        }
     }
 }
